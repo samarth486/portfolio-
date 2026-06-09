c0425e8364735b18f1a6a8433a97a077695fb0fe
samarth486 <samarths9393@gmail.com>
2026-06-09 15:50:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const links = document.querySelectorAll('nav a');
+
+links.forEach(link => {
+    link.addEventListener('click', function(e){
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
